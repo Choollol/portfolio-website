9d7813d770b6c3e1e0eb565131e86f3c411d72f8
Choollol <67297627+Choollol@users.noreply.github.com>
2025-02-13 10:39:08 -0800

    Add base url prefix

--- src/utils/PageUtils.tsx
+++ src/utils/PageUtils.tsx
@@ -1,10 +1,12 @@
 // ABS suffix: Absolute path
 // REL suffix: Relative path 
 
-export const HOME_PAGE_PATH_ABS: string = "/";
-export const GAMES_PAGE_PATH_ABS: string = "/games";
-export const PROJECTS_PAGE_PATH_ABS: string = "/projects";
-export const LINKS_PAGE_PATH_ABS: string = "/links";
+export const BASE_URL = "portfolio-website";
+
+export const HOME_PAGE_PATH_ABS: string = BASE_URL + "/";
+export const GAMES_PAGE_PATH_ABS: string = BASE_URL + "/games";
+export const PROJECTS_PAGE_PATH_ABS: string = BASE_URL + "/projects";
+export const LINKS_PAGE_PATH_ABS: string = BASE_URL + "/links";
 
 export type PageSummaryInfo = {
   imagePath: string;
