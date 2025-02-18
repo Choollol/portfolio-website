3ff7c15081c7e3046457cb809cffa7a62dbe500f
Choollol <67297627+Choollol@users.noreply.github.com>
2025-02-18 12:46:58 -0800

    Add sleep lab paragraph to home page and add footer border

--- src/data/page-data/home-page-data.tsx
+++ src/data/page-data/home-page-data.tsx
@@ -5,6 +5,8 @@ import cppLogo from "@/assets/images/icons/CPP_Logo.png";
 import reactLogo from "@/assets/images/icons/React_Logo.png";
 import unityLogo from "@/assets/images/icons/Unity_Logo.png";
 import csharpLogo from "@/assets/images/icons/CSharp_Logo.png";
+import minecraftLogo from "@/assets/images/icons/Minecraft_Logo.webp";
+import javaLogo from "@/assets/images/icons/Java_Logo.png";
 
 export let introductionData: AlternatingSectionInfo = {
   title: "Introduction",
@@ -12,12 +14,12 @@ export let introductionData: AlternatingSectionInfo = {
     {
       text: (
         <>
-          I'm Christopher Sun, a first-year Computer Science major at UC
-          Irvine. Some programming languages I know are C#, C, C++, Java,
-          Python, and JavaScript/TypeScript. I have been developing games in
-          Unity for a little over two years, and I'm also coding personal
-          projects in on my own time. One of those projects is this website,
-          which I am developing using React and Vite.
+          I'm Christopher Sun, a first-year Computer Science major at UC Irvine.
+          Some programming languages I know are C#, C, C++, Java, Python, and
+          JavaScript/TypeScript. I have been developing games in Unity for a
+          little over two years, and I'm also coding personal projects in on my
+          own time. One of those projects is this website, which I am developing
+          using React and Vite.
         </>
       ),
       imageInfo: [
@@ -53,6 +55,30 @@ export let currentProjectsData: AlternatingSectionInfo = {
         },
         {
           imagePath: csharpLogo,
+          imageSize: ImageSize.SMALL,
+        },
+      ],
+    },
+    {
+      text: (
+        <>
+          I'm also a Research Assistant at the{" "}
+          <a href="https://sleepinpediatrics.com/">
+            Sleep, Learning, and Emotion in Pediatrics (SLEEP) Lab
+          </a>
+          &nbsp;here at UC Irvine. I am aiding in the development of their
+          MindCycle mobile app, created with Unity, to help collect data. I am
+          also developing Minecraft plugins in Java for the lab to help test
+          spatial awareness and memory.
+        </>
+      ),
+      imageInfo: [
+        {
+          imagePath: minecraftLogo,
+          imageSize: ImageSize.SMALL,
+        },
+        {
+          imagePath: javaLogo,
           imageSize: ImageSize.SMALL,
         },
       ],
