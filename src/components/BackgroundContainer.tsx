cf3c89be062dd1c9936129d7dba0292c3ff98626
Choollol <67297627+Choollol@users.noreply.github.com>
2025-11-05 15:41:14 -0800

    Fix: Prevent BackgroundContainer from being selected

--- src/components/BackgroundContainer.tsx
+++ src/components/BackgroundContainer.tsx
@@ -1,3 +1,4 @@
+import { createStyles } from "@/styles/styling";
 import { Box } from "@mui/material";
 import { ReactNode } from "react";
 
@@ -5,19 +6,18 @@ interface Props {
   children: ReactNode;
 }
 
+const styles = createStyles({
+  container: {
+    position: "absolute",
+    width: "100vw",
+    height: "100%",
+    clipPath: "inset(0 0 0 0)",
+    pointerEvents: "none",
+  },
+});
+
 const BackgroundContainer = ({ children }: Props) => {
-  return (
-    <Box
-      sx={{
-        position: "absolute",
-        width: "100vw",
-        height: "100%",
-        clipPath: "inset(0 0 0 0)",
-      }}
-    >
-      {children}
-    </Box>
-  );
+  return <Box sx={styles.container}>{children}</Box>;
 };
 
 export default BackgroundContainer;
